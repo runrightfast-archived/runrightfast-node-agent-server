b96c32bd7eea6d52bd4026c50a8e4ce614904a06
Alfio Zappala <azappala@azaptree.com>
2013-10-07 21:40:10 -0400

    started adding unit tests

---
+++ test/server.js
@@ -0,0 +1,131 @@
+/**
+ * Copyright [2013] [runrightfast.co]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+'use strict';
+
+var expect = require('chai').expect;
+var lodash = require('lodash');
+var Hapi = require('hapi');
+var serverConfig = require('../config');
+var config = require('config');
+
+var restClient = null;
+
+var initRestClient = function(callback) {
+	var options = {
+		baseUrl : 'http://localhost:7000',
+		auth : {
+			hawk : {
+				credentials : {
+					id : 'dh37fgj492je',
+					key : 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn',
+					algorithm : 'sha256'
+				}
+			}
+		}
+	};
+
+	var cbConnManager = require('runrightfast-couchbase').couchbaseConnectionManager;
+	var HawkAuthService = require('runrightfast-auth-service').HawkAuthService;
+
+	var hawkAuthService = new HawkAuthService({
+		couchbaseConn : cbConnManager.getBucketConnection(config.hapiServer.auth.hawk.couchbaseBucket),
+		logLevel : config.hapiServer.auth.hawk.logLevel
+	});
+
+	hawkAuthService.createCredentials(function(err, credentials) {
+		if (err) {
+			callback(err);
+		} else {
+			options.auth.hawk.credentials = credentials;
+			restClient = require('runrightfast-rest-client')(options);
+			callback();
+		}
+	});
+
+};
+
+var fs = require('fs');
+var file = require('file');
+var path = require('path');
+
+var logDir = file.path.abspath('temp/logs');
+
+describe('Node Agent Server', function() {
+	var composer = null;
+
+	before(function(done) {
+		composer = new Hapi.Composer(serverConfig.manifest);
+
+		composer.compose(function(err) {
+			if (err) {
+				console.error('Failed composing servers : ' + err.message);
+				callback(err);
+			} else {
+				console.log('Hapi is composed.');
+				composer.start(function() {
+					console.log('All servers started');
+					file.mkdirs(logDir, parseInt('0755', 8), function(err) {
+						if (err) {
+							done(err);
+						} else {
+							initRestClient(done);
+						}
+					});
+				});
+			}
+		});
+	});
+
+	after(function(done) {
+		if (composer) {
+			composer.stop({
+				timeout : 1000
+			}, function() {
+				console.log('All servers stopped');
+				done();
+			});
+		}
+	});
+
+	it('POST /api/process-monitor-logs/logManager/logDir', function(done) {
+		var payload = {
+			logDir : logDir
+		};
+
+		var restRequest = {
+			path : '/api/process-monitor-logs/logManager/logDir',
+			entity : payload,
+			headers : {
+				"Content-Type" : 'application/json'
+			}
+		};
+
+		restClient(restRequest).then(function(response) {
+			console.log(response.headers);
+			expect(response.status.code).to.equal(201);
+			done();
+		}, function(response) {
+			var info = {
+				statusCode : response.status.code,
+				statusText : response.status.text,
+				entity : response.entity
+			};
+			done(new Error('log request failed: ' + JSON.stringify(info)));
+		});
+	});
+
+});
